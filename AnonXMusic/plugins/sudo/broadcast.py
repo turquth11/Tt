17a57842c3ee2750481e7ce24777985a22e43ca6
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2024-03-14 16:35:25 +0300

    Update broadcast.py

--- AnonXMusic/plugins/sudo/broadcast.py
+++ AnonXMusic/plugins/sudo/broadcast.py
@@ -179,7 +179,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_2"])
         text = _["broad_3"]
-        from AlexaMusic.core.userbot import assistants
+        from AnonXMusic.core.userbot import assistants
 
         for num in assistants:
             sent = 0
