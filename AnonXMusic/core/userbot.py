29dfa066506563018f7d00958ef2c2128df8d64b
mamakli06 <83599873+mamakli06@users.noreply.github.com>
2024-04-09 20:01:05 +0300

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("mamaklimekani")
+                await self.one.join_chat("mamaklibirininruhu")
             except:
                 pass
             assistants.append(1)
